218c943c63bbedf63037cd461bf987cd486440a2
Света <box4svv@yandex.ru>
2021-02-11 09:38:03 +0300

    Задача №2 "Типы данных"

---
+++ DataTypes.java
@@ -0,0 +1,12 @@
+package Tasks;
+
+public class DataTypes {
+    public static void main(String[] args) {
+        double a = 2.65;
+        System.out.println(a);
+
+        byte b = 127;
+        System.out.println(b);
+
+    }
+}
