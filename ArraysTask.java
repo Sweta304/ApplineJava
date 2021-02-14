98822218075ab9601227389b78246bb8552cf8cd
Света <box4svv@yandex.ru>
2021-02-14 21:17:52 +0300

    Задача №3 "Массивы"

--- ArraysTask.java
+++ ArraysTask.java
@@ -7,8 +7,8 @@ public class ArraysTask {
         int [] someArray = {10,23,21,12,39};
         int temp = someArray[0];
         someArray[0]=someArray[4];
-        someArray[4]=temp;
-        System.out.println(someArray[0]+someArray[2]);
+        someArray[someArray.length-1]=temp;
+        System.out.println(someArray[0]+someArray[someArray.length/2]);
     }
 
 }
