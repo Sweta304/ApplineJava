5093537f1c9e18020c680855a9e73b50c46d2c5b
Sweta304 <56889984+Sweta304@users.noreply.github.com>
2021-02-21 19:26:55 +0300

    Add files via upload

---
+++ Operators.java
@@ -0,0 +1,24 @@
+package Tasks;
+
+import java.util.Scanner;
+
+public class Operators {
+    public static void main(String[] args) {
+        //ввести 3 числа с клавиатуры
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("введите число x: ");
+        int x = scanner.nextInt();
+        System.out.print("введите число y: ");
+        int y = scanner.nextInt();
+        System.out.print("введите число z: ");
+        int z = scanner.nextInt();
+        //среднее арифметическое x,y,z
+        double avg = (x+y+z)/3;
+        //деление среднего арифметического на 2 и округление вниз
+        double div = Math.floor(avg/2);
+        //условие "если div>3"
+        String answer = (div>3)?"Программа выполнена корректно":" ";
+        System.out.println(answer);
+
+    }
+}
---
+++ OperatorsPartTwo.java
@@ -0,0 +1,35 @@
+package Tasks;
+
+import java.util.Scanner;
+
+public class OperatorsPartTwo {
+    public static void main(String[] args) {
+        //ввести 2 числа и символ с клавиатуры
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("введите число x: ");
+        double x = scanner.nextInt();
+        System.out.print("введите число y: ");
+        double y = scanner.nextInt();
+        System.out.print("введите арифметическое действие: ");
+        String z = scanner.next();
+        //калькулятор
+        switch(z) {
+            case "+":
+                System.out.print(x+y);
+                break;
+            case "-":
+                System.out.print(x-y);
+                break;
+            case "*":
+                System.out.print(x*y);
+                break;
+            case "/":
+                System.out.print(x/y);
+                break;
+            default:
+                System.out.print("действие невозможно, введите одну из доступных операций: +, -, *, /");
+                break;
+        }
+
+    }
+}
