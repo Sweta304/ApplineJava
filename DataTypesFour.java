5b0b6cb0c844a0869413a325d99553e2e725c3a8
Света <box4svv@yandex.ru>
2021-03-07 14:31:09 +0300

    Задача №14 "Работа с данными"

---
+++ DataTypesFour.java
@@ -0,0 +1,32 @@
+package Tasks;
+
+import java.util.Random;
+
+public class DataTypesFour {
+    public static void main(String[] args) {
+        Random random = new Random();
+
+        int[] someArray = new int[15];
+        //вывод в консоль всех элементов массива
+        for (int i = 0; i < someArray.length; i++) {
+            int result = 20 - random.nextInt(40);
+            someArray[i] = result;
+            System.out.print(result+"; ");
+        }
+        System.out.println();
+
+        //вывод в консоль максимально и минимального элементов
+        int max = 0;
+        int min = 0;
+        for (int k=0;k<someArray.length;k++){
+            max = Math.max(max,someArray[k]);
+            min = Math.min(min,someArray[k]);
+        }
+        System.out.print("Максимальный элемент массива: ");
+        System.out.println(max);
+        System.out.print("Минимальный элемент массива: ");
+        System.out.println(min);
+        System.out.print("Наибольший элемент по модулю: ");
+        System.out.println(Math.max(Math.abs(max),Math.abs(min)));
+    }
+}
